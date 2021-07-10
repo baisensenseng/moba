f5ecfda9c61996c12f93f97941ebcf4715ba1b9a
xierongfei <xierongfei@aegis-data.cn>
2021-07-10 19:28:32 +0800

    main

---
+++ web/src/views/Main.vue
@@ -0,0 +1,25 @@
+<template lang="">
+  <div>
+    <div class="topbar bg-black py-1 text-white px-3">
+      <!-- topbar -->
+      <img src="../assets/images/logo.png" alt="" height='30'>
+    </div>
+    <div class="nav">
+      nav
+    </div>
+    <router-view></router-view>
+  </div>
+  
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+<style lang="">
+  
+</style>
