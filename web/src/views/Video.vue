1b3ea4c10acd8f99caa30069de76801485d10861
xierongfei <xierongfei@aegis-data.cn>
2021-08-26 17:12:58 +0800

    添加新增视频模块 fixbug

--- web/src/views/Video.vue
+++ web/src/views/Video.vue
@@ -14,7 +14,7 @@
     <h1>{{model.name}}</h1>
     <!-- <video src="" controls></video> -->
     <video :src="model.url" style="width: 100%" controls="controls"></video>
-    <video :src="model.url" controls="controls" class="video-style" webkit-playsinline="true" playsinline="true"
+    <video :src="model.url" style="width: 100%" controls="controls" class="video-style" webkit-playsinline="true" playsinline="true"
     x5-video-player-type="h5" x5-video-player-fullscreen="true">
     </video>
   </div>
