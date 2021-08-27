b47eb6f8de864ff7b908fc997f41feaa625f6d1b
xierongfei <xierongfei@aegis-data.cn>
2021-08-27 14:31:02 +0800

    添加新增视频模块 fixbug

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -76,26 +76,26 @@ module.exports = app =>{
 
   app.use("/admin/api/rest/:resource", authMiddleware(), resourceMiddleware(),router);
 
-  const fs = require('fs')
+  // const fs = require('fs')
   const multer = require('multer')
   // const upload = multer({ dest: __dirname + '/../../uploads' })
 
   // 更改大文件的存储路径
-  const createFolder = function(folder){
-    try{
-      fs.accessSync(folder);
-    }catch( e ){
-      fs.mkdirSync(folder);
-    }
-  };
-  const uploadFolder = `${__dirname} +  /../../uploads`
+  // const createFolder = function(folder){
+  //   try{
+  //     fs.accessSync(folder);
+  //   }catch( e ){
+  //     fs.mkdirSync(folder);
+  //   }
+  // };
+  // const uploadFolder = `${__dirname} +  /../../uploads`
 
-  createFolder(uploadFolder);
+  // createFolder(uploadFolder);
 
   // 通过 filename 属性定制
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, uploadFolder);    // 保存的路径，备注：需要自己创建
+        cb(null, `${__dirname} +  /../../uploads`);    // 保存的路径，备注：需要自己创建
     },
     filename: function (req, file, cb) {
         // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
