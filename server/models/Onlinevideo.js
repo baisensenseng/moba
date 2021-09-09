94fc482c2a41d0f815b03d30a7fe6d89572f61b4
xierongfei <xierongfei@aegis-data.cn>
2021-09-09 15:13:41 +0800

    models

--- server/models/Onlinevideo.js
+++ server/models/Onlinevideo.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
   title: { type: String },
   time: { type: String },
+  videourl: { type: String },
   // categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
   body: { type: String },
 },{
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -90,6 +90,13 @@ module.exports = app =>{
   const upload = multer({ storage: storage })
 
 
+  app.post('/admin/api/onlinevideoupload', upload.single('file'), async (req, res) => {
+    const file = req.file
+    file.url = `http://moba.xrfclub.com/uploads/${file.filename}`
+    // file.url = `http://localhost:3000/uploads/${file.filename}`
+    res.send(file)
+  })
+
   app.post('/admin/api/upload', upload.single('file'), authMiddleware(), async (req, res) => {
     const file = req.file
     file.url = `http://moba.xrfclub.com/uploads/${file.filename}`
