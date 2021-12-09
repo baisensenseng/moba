9450a2065110ef3418d1c48222a30a33a0bfe35a
xierongfei <xierongfei@aegis-data.cn>
2021-12-09 13:55:55 +0800

    unlock-music

---
+++ server/unlockmusic/precache-manifest.2f8f993fdc168f47c47c0079fe6d2a9b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bed28275ddd33cd041",
+    "url": "unlockmusic/css/app.084e4317.css"
+  },
+  {
+    "revision": "b03b885058ee85289f94",
+    "url": "unlockmusic/css/chunk-vendors.d21c70ba.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "unlockmusic/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "unlockmusic/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "0231da5f3a41dc8c2facf09b719f55b8",
+    "url": "unlockmusic/index.html"
+  },
+  {
+    "revision": "2d12777f2703612307ff4a12f1b21899",
+    "url": "unlockmusic/ixarea-stats.js"
+  },
+  {
+    "revision": "761cd14c4ea3e66338c1dbc669bd293d",
+    "url": "unlockmusic/js/0.4f83b29b.worker.js"
+  },
+  {
+    "revision": "f2bed28275ddd33cd041",
+    "url": "unlockmusic/js/app.48216636.js"
+  },
+  {
+    "revision": "b03b885058ee85289f94",
+    "url": "unlockmusic/js/chunk-vendors.3deb504f.js"
+  },
+  {
+    "revision": "02995355b96ddf2519cd49f8aa73bb46",
+    "url": "unlockmusic/loader.js"
+  },
+  {
+    "revision": "523b1a2eae8cb533fa6bd73831308f09",
+    "url": "unlockmusic/static/kgm.mask"
+  },
+  {
+    "revision": "cd1d395410107c66b4534ec93f0073d3",
+    "url": "unlockmusic/web-manifest.json"
+  }
+]);
