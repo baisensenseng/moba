c945430d530aba48d0a4dd69a74bbeaab57bc082
xierongfei <xierongfei@aegis-data.cn>
2021-12-09 14:01:06 +0800

    unlock-music

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.use(require('cors')())
 app.use(express.json())
 app.use('/', express.static(__dirname + '/web'))
 app.use('/admin', express.static(__dirname + '/admin'))
+app.use('/unlockmusic', express.static(__dirname + '/unlockmusic'))
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
 require('./plugins/db')(app) // 引用数据库
